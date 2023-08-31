09217a04ec4172edae9af0a1cbac9557bdd1d437
abgox <abgohxf@outlook.com>
2023-08-31 08:39:26 +0800

    feat(completions): Update PSCompletions

--- completions/PSCompletions/PSCompletions.ps1
+++ completions/PSCompletions/PSCompletions.ps1
@@ -13,14 +13,10 @@ Register-ArgumentCompleter -CommandName $_psc.comp_cmd.PSCompletions -ScriptBloc
     # #endregion
 
     #region : Store
-    $max_len = 0
     foreach ($_ in $_json) {
-        $subCmd = $_.Name.substring($_.Name.lastIndexOf(' ') + 1)
-        if ($max_len -lt $subCmd.length) {
-            $max_len = $subCmd.length
-        }
         if ($_.Name -ne 'PSCompletions_core_info') {
-            $completions[ $root_cmd + ' ' + $_.Name] = [CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', (_psc_replace $_.value))
+            $subCmd = $_.Name.substring($_.Name.lastIndexOf(' ') + 1)
+            $completions[ $root_cmd + ' ' + $_.Name] = @([CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', (_psc_replace $_.value)), $subCmd.length, ($_.Value -split "`n").Count)
         }
     }
     #endregion
@@ -28,93 +24,122 @@ Register-ArgumentCompleter -CommandName $_psc.comp_cmd.PSCompletions -ScriptBloc
     #region Special point
     foreach ($_ in $_psc.list) {
         if ($_ -notin $_psc.comp_cmd.keys) {
-            $completions[ $root_cmd + ' add ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace $_psc.json.add) )
+            $tip = _psc_replace $_psc.json.add
+            $completions[ $root_cmd + ' add ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip), $_.Length, ($tip -split "`n").Count)
         }
     }
     if ($_psc.update) {
         foreach ($_ in $_psc.update) {
-            $completions[ $root_cmd + ' update ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace $_psc.json.update))
+            $tip = _psc_replace $_psc.json.update
+            $completions[ $root_cmd + ' update ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip), $_.Length, ($tip -split "`n").Count)
         }
     }
     foreach ($_ in $_psc.comp_cmd.keys) {
-        $completions[$root_cmd + ' rm ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace $_psc.json.remove))
-        $completions[$root_cmd + ' which ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace $_psc.json.which))
-        $completions[$root_cmd + ' alias add ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace $_psc.json.alias_add))
         $alias = $_psc.comp_cmd.$_
+        $tip_rm = _psc_replace $_psc.json.remove
+        $completions[$root_cmd + ' rm ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip_rm), $_.Length, ($tip_rm -split "`n").Count)
+
+        $tip_which = _psc_replace $_psc.json.which
+        $completions[$root_cmd + ' which ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip_which), $_.Length, ($tip_which -split "`n").Count)
+
+        $tip_alias_add = _psc_replace $_psc.json.alias_add
+        $completions[$root_cmd + ' alias add ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip_alias_add), $_.Length, ($tip_alias_add -split "`n").Count)
+
         if (!($_ -eq 'PSCompletions' -and $alias -eq 'psc')) {
-            $completions[$root_cmd + ' alias rm ' + $alias] = [CompletionResult]::new($alias, $alias, 'ParameterValue', (_psc_replace $_psc.json.alias_rm))
+            $tip_alias_rm = _psc_replace $_psc.json.alias_rm
+            $completions[$root_cmd + ' alias rm ' + $alias] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip_alias_rm), $_.Length, ($tip_alias_rm -split "`n").Count)
         }
     }
     foreach ($_ in @('language', 'root_cmd', 'github', 'gitee', 'update')) {
-        $completions[$root_cmd + ' config ' + $_] = [CompletionResult]::new($_, $_, 'ParameterValue', (_psc_replace ($_psc.json.config + $json.('config ' + $_))))
+        $tip = _psc_replace $json.('config ' + $_)
+        $completions[$root_cmd + ' config ' + $_] = @([CompletionResult]::new($_, $_, 'ParameterValue', $tip), $_.Length, ($tip -split "`n").Count)
     }
     #endregion
 
     #region : Carry out
-    $comp_num = ([System.Console]::WindowHeight - 2) * ([math]::Floor([System.Console]::WindowWidth / ($max_len + 2)))
     $_input = $commandAst.CommandElements
-    function _do($num) {
-        $i = 0
-        $completions.Keys | Where-Object { $_ -like "$_input*" } | ForEach-Object {
-            $input_space_count = ($_input -split ' ').Count - 1
-            $cmd_space_count = ($_ -split ' ').Count - 1
-            if ($input_space_count -eq $cmd_space_count + $num ) {
-                $i++
-                if ($comp_num -gt $i) { $completions[$_] }
-                else {
-                    [CompletionResult]::new(" ", "...", 'ParameterValue', "...")
-                    return
-                }
-            }
+    $cmd_line = [System.Console]::WindowHeight - 4
+    $limit_max = @(0, 0)
+    $count = 0
+    $display_count = 0
+    $input_tab = if ($wordToComplete.length) { $true }else { $false }
+    $input_count = $_input.Count
+    $all = $completions.Keys | Where-Object {
+        $cmd = $_ -split '\s+'
+        $temp = $cmd[0..($input_count - 1)]
+        if ($input_tab) {
+            $cmd.Count -eq $input_count -and $temp -join ' ' -like ($_input -join ' ') + '*'
         }
-        if ($i -eq 1) { Write-Output ' ' }
+        else {
+            $cmd.Count -eq ($input_count + 1) -and ($temp -join ' ') -eq $_input
+        }
+    } | foreach-Object {
+        if ($completions[$_][1] -ge $limit_max[0]) { $limit_max[0] = $completions[$_][1] }
+        if ($completions[$_][2] -ge $limit_max[1]) { $limit_max[1] = $completions[$_][2] }
+        $count++
+        $_
     }
-    _do $(if ($wordToComplete.length) { 0 }else { -1 })
+    $comp_count = ($cmd_line - $limit_max[1] ) * [math]::Floor([System.Console]::WindowWidth / ($limit_max[0] + 2))
+    if ($comp_count -le [math]::Floor($count * 2 / 3)) {
+        $comp_count = $cmd_line * [math]::Floor([System.Console]::WindowWidth / ($limit_max[0] + 2))
+    }
+    $all | ForEach-Object {
+        $display_count++;
+        if ($comp_count -gt $display_count) { $completions[$_][0] }
+        else {
+            [CompletionResult]::new(" ", "...", 'ParameterValue', "...")
+            return
+        }
+    }
+    if ($display_count -eq 1) { echo ' ' }
     #endregion
 
     #region Reorder completion
     $history = try { (Get-History)[-1].CommandLine }catch { '' }
-    $null = Start-Job -ScriptBlock {
-        param( $_psc, $history, $root)
-        function _do($flag, $path, $res = [ordered]@{}) {
-            $json = Get-Content $path -Raw -Encoding UTF8 | ConvertFrom-Json
-            $res = [ordered]@{}
-            $res_flag = @()
-            foreach ($_ in $json.PSObject.Properties) {
-                $type = ($_.value).GetType().Name
-                if ($type -eq 'String') {
-                    $i = $flag
-                    while ($i) {
-                        if ($_.Name -eq $i) { $res_flag += $_.Name }
-                        if ( $i.lastIndexOf(' ') -eq -1) { break }
-                        $i = $i.Substring(0, $i.lastIndexOf(' '))
+    function reorder($_psc, $PSScriptRoots, $history) {
+        $null = Start-Job -ScriptBlock {
+            param( $_psc, $history, $root)
+            function _do($flag, $path, $res = [ordered]@{}) {
+                $json = Get-Content $path -Raw -Encoding UTF8 | ConvertFrom-Json
+                $res = [ordered]@{}
+                $res_flag = @()
+                foreach ($_ in $json.PSObject.Properties) {
+                    $type = ($_.value).GetType().Name
+                    if ($type -eq 'String') {
+                        $i = $flag
+                        while ($i) {
+                            if ($_.Name -eq $i) { $res_flag += $_.Name }
+                            if ( $i.lastIndexOf(' ') -eq -1) { break }
+                            $i = $i.Substring(0, $i.lastIndexOf(' '))
+                        }
                     }
                 }
-            }
-            $res_arr = @()
-            foreach ($_ in $json.PSObject.Properties) {
-                $type = ($_.value).GetType().Name
-                if ($type -eq 'String') {
-                    if ($_.Name -in $res_flag) {
-                        $res_arr += @{cmd = $_.Name; value = $_.value; len = ($_.Name).Length }
+                $res_arr = @()
+                foreach ($_ in $json.PSObject.Properties) {
+                    $type = ($_.value).GetType().Name
+                    if ($type -eq 'String') {
+                        if ($_.Name -in $res_flag) {
+                            $res_arr += @{cmd = $_.Name; value = $_.value; len = ($_.Name).Length }
+                        }
+                        else { $res.($_.Name) = $_.value }
                     }
                     else { $res.($_.Name) = $_.value }
                 }
-                else { $res.($_.Name) = $_.value }
-            }
 
-            $res_arr | Sort-Object { $_.len } -Descending | ForEach-Object {
-                $res.Insert(0, $_.cmd, $_.value)
+                $res_arr | Sort-Object { $_.len } -Descending | ForEach-Object {
+                    $res.Insert(0, $_.cmd, $_.value)
+                }
+                $res | ConvertTo-Json | Out-File $path
             }
-            $res | ConvertTo-Json | Out-File $path
-        }
-        if ($history -ne '') {
-            $cmd = $history -split ' '
-            $alias = $_psc.comp_cmd.keys | foreach-Object { $_psc.comp_cmd.$_ }
-            if ($cmd[0] -in $alias) {
-                _do $history.Substring($history.IndexOf(' ') + 1) ($root + '\json\' + $_psc.lang + '.json')
+            if ($history -ne '') {
+                $cmd = $history -split ' '
+                $alias = $_psc.comp_cmd.keys | foreach-Object { $_psc.comp_cmd.$_ }
+                if ($cmd[0] -in $alias) {
+                    _do $history.Substring($history.IndexOf(' ') + 1) ($root + '\json\' + $_psc.lang + '.json')
+                }
             }
-        }
-    } -ArgumentList $_psc, $history, $PSScriptRoot
+        } -ArgumentList $_psc, $history, $PSScriptRoots
+    }
+    # reorder $_psc $PSScriptRoot $history
     #endregion
 }
