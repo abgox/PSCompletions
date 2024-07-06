009d2909b6761b4389cceb11ae186867aec56247
abgox <abgohxf@outlook.com>
2024-07-06 18:50:09 +0800

    feat(docs): update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,19 +1,21 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+// import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import Layout from "./layout.vue"
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  // Layout: () => {
+  //   return h(DefaultTheme.Layout, null, {
+  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
+  //   })
+  // },
+  Layout,
   enhanceApp() {
     try {
-      let langs = ['en-US', 'zh-CN']
+      const langs = ['en-US', 'zh-CN']
       let lang = navigator.language
       if (!langs.includes(lang)) {
         lang = langs[0]
