8f5b47030fafce4490bef6bf27ab99db04b3079d
abgox <abgohxf@outlook.com>
2025-10-31 23:52:13 +0800

    feat(completions): update some completions

--- completions/scoop-install/hooks.ps1
+++ completions/scoop-install/hooks.ps1
@@ -59,34 +59,34 @@
         else {
             if ($CN) {
                 $tips = @{
-                    '-g'                = "U: -g|--global`n全局安装程序"
-                    '--global'          = "U: -g|--global`n全局安装程序"
-                    '-i'                = "U: -i|--independent`n不自动安装依赖项"
-                    '--independent'     = "U: -i|--independent`n不自动安装依赖项"
-                    '-k'                = "U: -k|--no-cache`n不使用下载缓存"
-                    '--no-cache'        = "U: -k|--no-cache`n不使用下载缓存"
-                    '-s'                = "U: -s|--skip-hash-check`n跳过哈希验证(使用时请谨慎!)"
-                    '--skip-hash-check' = "U: -s|--skip-hash-check`n跳过哈希验证(使用时请谨慎!)"
-                    '-u'                = "U: -u|--no-update-scoop`n安装之前不更新 Scoop，如果它已过时"
-                    '--no-update-scoop' = "U: -u|--no-update-scoop`n安装之前不更新 Scoop，如果它已过时"
-                    '-a'                = "U: -a|--arch`n使用指定的体系结构，如果程序支持"
-                    '--arch'            = "U: -a|--arch`n使用指定的体系结构，如果程序支持"
+                    '-g'                = "U: -g, --global`n全局安装程序"
+                    '--global'          = "U: -g, --global`n全局安装程序"
+                    '-i'                = "U: -i, --independent`n不自动安装依赖项"
+                    '--independent'     = "U: -i, --independent`n不自动安装依赖项"
+                    '-k'                = "U: -k, --no-cache`n不使用下载缓存"
+                    '--no-cache'        = "U: -k, --no-cache`n不使用下载缓存"
+                    '-s'                = "U: -s, --skip-hash-check`n跳过哈希验证(使用时请谨慎!)"
+                    '--skip-hash-check' = "U: -s, --skip-hash-check`n跳过哈希验证(使用时请谨慎!)"
+                    '-u'                = "U: -u, --no-update-scoop`n安装之前不更新 Scoop，如果它已过时"
+                    '--no-update-scoop' = "U: -u, --no-update-scoop`n安装之前不更新 Scoop，如果它已过时"
+                    '-a'                = "U: -a, --arch`n使用指定的体系结构，如果程序支持"
+                    '--arch'            = "U: -a, --arch`n使用指定的体系结构，如果程序支持"
                 }
             }
             else {
                 $tips = @{
-                    '-g'                = "U: -g|--global`nInstall the app globally."
-                    '--global'          = "U: -g|--global`nInstall the app globally."
-                    '-i'                = "U: -i|--independent`nDon't install dependencies automatically."
-                    '--independent'     = "U: -i|--independent`nDon't install dependencies automatically."
-                    '-k'                = "U: -k|--no-cache`nDon't use the download cache."
-                    '--no-cache'        = "U: -k|--no-cache`nDon't use the download cache."
-                    '-s'                = "U: -s|--skip-hash-check`nSkip hash validation (use with caution!)."
-                    '--skip-hash-check' = "U: -s|--skip-hash-check`nSkip hash validation (use with caution!)."
-                    '-u'                = "U: -u|--no-update-scoop`nDon't update Scoop before installing if it's outdated."
-                    '--no-update-scoop' = "U: -u|--no-update-scoop`nDon't update Scoop before installing if it's outdated."
-                    '-a'                = "U: -a|--arch`nUse the specified architecture, if the app supports it."
-                    '--arch'            = "U: -a|--arch`nUse the specified architecture, if the app supports it."
+                    '-g'                = "U: -g, --global`nInstall the app globally."
+                    '--global'          = "U: -g, --global`nInstall the app globally."
+                    '-i'                = "U: -i, --independent`nDon't install dependencies automatically."
+                    '--independent'     = "U: -i, --independent`nDon't install dependencies automatically."
+                    '-k'                = "U: -k, --no-cache`nDon't use the download cache."
+                    '--no-cache'        = "U: -k, --no-cache`nDon't use the download cache."
+                    '-s'                = "U: -s, --skip-hash-check`nSkip hash validation (use with caution!)."
+                    '--skip-hash-check' = "U: -s, --skip-hash-check`nSkip hash validation (use with caution!)."
+                    '-u'                = "U: -u, --no-update-scoop`nDon't update Scoop before installing if it's outdated."
+                    '--no-update-scoop' = "U: -u, --no-update-scoop`nDon't update Scoop before installing if it's outdated."
+                    '-a'                = "U: -a, --arch`nUse the specified architecture, if the app supports it."
+                    '--arch'            = "U: -a, --arch`nUse the specified architecture, if the app supports it."
                 }
             }
             foreach ($param in $paramList) {
