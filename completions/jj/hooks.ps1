16dd1c48d86cca17404fe373437c4a596864e612
abgox <abgohxf@outlook.com>
2025-11-03 00:10:54 +0800

    feat(completions): update jj

--- completions/jj/hooks.ps1
+++ completions/jj/hooks.ps1
@@ -1,18 +1,22 @@
 function handleCompletions($completions) {
     $list = @()
 
-    # $input_arr = $PSCompletions.input_arr
+    $input_arr = $PSCompletions.input_arr
     $filter_input_arr = $PSCompletions.filter_input_arr # Without -*
 
     function return_bookmark {
-        return jj bookmark list 2>$null | ForEach-Object { $_.split(':')[0] }
+        return jj bookmark list 2>$null | ForEach-Object {
+            if ($_ -match '^[^\s:]+:') {
+                $_.split(':')[0]
+            }
+        }
     }
 
     switch ($filter_input_arr[0]) {
         'new' {
             $bookmark_list = return_bookmark
             foreach ($_ in $bookmark_list) {
-                $list += $PSCompletions.return_completion($_, "Bookmark: " + $_)
+                $list += $PSCompletions.return_completion($_, "bookmark")
             }
         }
         'bookmark' {
@@ -20,11 +24,19 @@ function handleCompletions($completions) {
                 { $_ -in 'set', 'rename', 'move', 'forget', 'delete' } {
                     $bookmark_list = return_bookmark
                     foreach ($_ in $bookmark_list) {
-                        $list += $PSCompletions.return_completion($_, "Bookmark: " + $_)
+                        $list += $PSCompletions.return_completion($_, "bookmark")
                     }
                 }
             }
         }
     }
+
+    if ($input_arr[-1] -cin '-r', '--revisions', '-b', '--branch') {
+        $bookmark_list = return_bookmark
+        foreach ($_ in $bookmark_list) {
+            $list += $PSCompletions.return_completion($_, "bookmark")
+        }
+    }
+
     return $list + $completions
 }
