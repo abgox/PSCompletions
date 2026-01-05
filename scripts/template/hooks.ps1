0d844cfb5fea56c9074e9e8a0e22f999e5273135
abgox <abgohxf@outlook.com>
2026-01-05 23:06:19 +0800

    feat(scripts): update

--- scripts/template/hooks.ps1
+++ scripts/template/hooks.ps1
@@ -2,12 +2,22 @@
 function handleCompletions($completions) {
     $list = @()
 
-    # $input_arr = $PSCompletions.input_arr
-    $filter_input_arr = $PSCompletions.filter_input_arr # Without -*
+    $input_arr = $PSCompletions.input_arr
+    $filter_input_arr = $PSCompletions.filter_input_arr # Exclude options parameters
+    $first_item = $filter_input_arr[0] # The first subcommand
+    $last_item = $filter_input_arr[-1] # The last subcommand
 
-    # switch ($filter_input_arr[0]) {
+    # switch ($first_item) {
     #     'add' {
-    #         $list += $PSCompletions.return_completion('aaa', "Add aaa")
+    #         if ('aaa' -notin $input_arr) {
+    #             $list += $PSCompletions.return_completion('aaa', "Add aaa")
+    #         }
+    #     }
+    # }
+
+    # switch ($last_item) {
+    #     'add' {
+    #         $list += $PSCompletions.return_completion('bbb', "Add bbb")
     #     }
     # }
 
